6ea686b3c9c7715f288483fcf997b90b61d621ec
latch0716 <latch0716@gmail.com>
2023-01-26 15:40:58 +0900

    「発送元の地域」の設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,8 +3,8 @@ class Item < ApplicationRecord
   validates :description, presence: true
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :shipping_cost_id, presence: true
-  validates :prefecture_id, presence: true
+  validates :shipping_cost_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :delivery_time_id, presence: true
   validates :price, presence: true
   validates :user, presence: true
@@ -13,4 +13,5 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :condition
   belongs_to :shipping_cost
+  belongs_to :prefecture
 end
