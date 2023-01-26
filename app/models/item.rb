901964d1d6b4c093d37ebe89cf7f9e67be8b85ec
latch0716 <latch0716@gmail.com>
2023-01-26 17:25:53 +0900

    価格のバリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,7 @@ class Item < ApplicationRecord
   validates :shipping_cost_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :delivery_time_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :price, presence: true
+  validates :price, presence: true, numericality: { in: 300..9999999 }
   validates :user, presence: true
 
   extend ActiveHash::Associations::ActiveRecordExtensions
