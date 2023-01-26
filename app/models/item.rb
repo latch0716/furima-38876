aab14ce13fb8505464137a891b9e9a1dcab3fd27
latch0716 <latch0716@gmail.com>
2023-01-26 15:24:10 +0900

    「商品の状態」の設定【修正1回目】

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   validates :product, presence: true
   validates :description, presence: true
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :condition_id, presence:true
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipping_cost_id, presence: true
   validates :prefecture_id, presence: true
   validates :delivery_time_id, presence: true
@@ -11,4 +11,5 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :condition
 end
