7de26e1f51cb6d36566de0c04813dc4345c371ab
latch0716 <latch0716@gmail.com>
2023-01-26 15:19:14 +0900

    カテゴリーの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,14 @@
 class Item < ApplicationRecord
   validates :product, presence: true
   validates :description, presence: true
-  validates :category_id, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :condition_id, presence:true
   validates :shipping_cost_id, presence: true
   validates :prefecture_id, presence: true
   validates :delivery_time_id, presence: true
   validates :price, presence: true
   validates :user, presence: true
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
 end
