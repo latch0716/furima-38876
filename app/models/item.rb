984bd7f85dbd19e70d97fb9c1df83cdfcf2966c8
latch0716 <latch0716@gmail.com>
2023-01-27 18:09:28 +0900

    商品出品ページの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
 class Item < ApplicationRecord
+  has_one_attached :image
+  belongs_to :user
+
+  validates :image, presence: true
   validates :product, presence: true
   validates :description, presence: true
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
